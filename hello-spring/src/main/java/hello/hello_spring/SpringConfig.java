34038159a9f2092df89c2c89c81b7b956862417e
ccjngwn <ahaha620@gmail.com>
2025-04-24 21:46:23 +0900

    Feat: spring bean 25.04.24

---
+++ hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -0,0 +1,21 @@
+package hello.hello_spring;
+
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository(); // 인터페이스는 등록이 안되므로 구현체
+    }
+}
