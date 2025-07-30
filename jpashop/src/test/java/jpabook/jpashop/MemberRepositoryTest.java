97e43b5116e934096d67f19306e26ce61ba4845f
ccjngwn <ahaha620@gmail.com>
2025-07-30 23:19:03 +0900

    섹션 2. 프로젝트 환경설정

---
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void testMember() throws Exception {
+
+    }
+
+
+}
