16a3822f78c6b4b64eb977a0b10eaf2f1c806ce1
ccjngwn <ahaha620@gmail.com>
2025-08-01 16:03:38 +0900

    섹션 3. 도메인 분석 설계

--- jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,4 +1,30 @@
 package jpabook.jpashop.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import static jakarta.persistence.FetchType.*;
+
+@Entity
+@Getter
+@Setter
 public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    // 이넘 넣을 때 이거 써줘야함.
+    // ORDINAL은 상태가 숫자로 들어감. 스트링으로 들어가게
+    // ORDINAL하면 중간에 새로운 상태가 생기면 망함
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; // READY, COMP
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,8 +1,11 @@
 package jpabook.jpashop.domain;
 
 import jakarta.persistence.*;
+import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
+
+import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Getter
@@ -14,9 +17,11 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
